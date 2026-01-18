e9a30f14dadcfbf1654e02ffd869b0274b724ee9
NBN TECH <contact@nbntechportfolio.com>
2026-01-18 22:51:33 +0100

    Fix migration dependency - enable database to apply whatsapp_url field

--- backend/content/migrations/0025_remove_old_fields_add_whatsapp.py
+++ backend/content/migrations/0025_remove_old_fields_add_whatsapp.py
@@ -6,7 +6,7 @@ from django.db import migrations, models
 class Migration(migrations.Migration):
 
     dependencies = [
-        ('content', '0024_productadmin'),
+        ('content', '0019_product_cover'),
     ]
 
     operations = [
